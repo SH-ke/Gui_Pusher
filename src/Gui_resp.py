4d9a631f33f029f774d79c2a5538c3b0748ce978
SH-ke <SHke_dev@163.com>
2022-09-18 00:52:37 +0800

    【调整】设定Gui_resp.py 模块 请求->数据处理->通知 的功能布局

--- src/Gui_resp.py
+++ src/Gui_resp.py
@@ -1,17 +1,39 @@
-import requests
+import requests, yaml, json
+from attrdict import AttrDict
+from fake_useragent import UserAgent
 
 
-url = "https://shop.laiyangni.com/api/lyn/wechat/home/shopInfo"
-parms = {
-    "isPs": 1,
-    "cityId": 110100,
-    "current": 1,
-    "lat": 40.22077,
-    "lon": 116.23128,
-    "size": 10,
-}
+def shop_resp() -> dict:
+    # 读取参数配置文件
+    yaml_file = "config/gui.yaml"
+    with open(yaml_file, 'rt', encoding="utf-8") as f:
+        args = AttrDict(yaml.safe_load(f))
 
-resp = requests.get(url, params=parms)
-print(resp.status_code)
-print(resp.json())
-resp.close()
+    ua = UserAgent()
+    HEADERS = {
+        "Content-Type": "application/json",
+        "User-Agent": ua.random,
+    }
+
+    # 发起 post 请求
+    resp = requests.post(args.resp.url, 
+                        data=json.dumps(args.resp.data), 
+                        headers=HEADERS)
+    data = json.dumps(resp.json()["data"]["records"], indent=4, 
+                        separators=(',', ': '), 
+                        ensure_ascii=False)
+    resp.close()
+    return data
+
+
+# 『数据预处理』 筛选满足条件的活动，整理成表格，保存为csv 存入数据库
+# 将surplus「剩余量」存入csv 数据库
+def data_preprocess(data: list):
+    pass
+
+
+# 『通知』 对比当前数据于上一次请求数据的差别，发送对应通知
+def notice_match():
+    # 「新活动」通知
+    # 「售罄」通知
+    pass
